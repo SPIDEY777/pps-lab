e60040f594a66bb7f1fcf47bae727d50985c6b50
SPIDEY777 <s733749@gmail.com>
2024-04-03 19:37:50 +0530

    19.c file added

---
+++ 19.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <math.h>
+int isarmstrong(int n); 
+int main()    
+{   
+    for(int i = 1; i <= 100;i++)
+    {
+        if(isarmstrong(i))
+        printf("%d",i);
+    }
+return 0;  
+}   
+
+int isarmstrong(int n) {
+
+    int m;int sum = 0; int c = 0;    
+
+    for(int i =n;i !=0;i=i/10) {
+    c++;
+}
+
+for(int i =n;i !=0;i=i/10) {
+    m = i%10;
+    sum = sum+ pow(m,c);
+
+}
+if(sum ==n)
+return 1;
+else
+return 0;
+}
